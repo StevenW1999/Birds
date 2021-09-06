0572b3128ec6f3cb9826752da3b1b4bcc9bdd35a
StevenW1999 <33688377+StevenW1999@users.noreply.github.com>
2021-09-06 12:54:37 +0200

    initial commit 2

---
+++ train.py
@@ -0,0 +1,11 @@
+import cv2     # for capturing videos
+import math   # for mathematical operations
+import matplotlib.pyplot as plt    # for plotting the images
+import pandas as pd
+from keras.preprocessing import image   # for preprocessing the images
+import numpy as np    # for mathematical operations
+from keras.utils import np_utils
+from skimage.transform import resize   # for resizing images
+from sklearn.model_selection import train_test_split
+from glob import glob
+from tqdm import tqdm
